1f49ed232d8cee28b3a1fc910d12ed1923438858
Saturn <pkomarek223@gmail.com>
2026-05-26 13:26:45 +0200

    Created game over screen

---
+++ HUD/GameOver.cs
@@ -0,0 +1,40 @@
+using Godot;
+using System;
+
+public partial class GameOver : CanvasLayer
+{
+	public override void _Ready()
+	{
+		// Na začátku bude skrytý
+		Visible = false;
+	}
+
+	public void ShowGameOver()
+	{
+		// Zobraz UI
+		Visible = true;
+
+		// Uvolní myš
+		Input.MouseMode = Input.MouseModeEnum.Visible;
+
+		// Pauzne hru
+		GetTree().Paused = true;
+	}
+
+	private void OnRestartButtonPressed()
+	{
+		// Odpauzne hru
+		GetTree().Paused = false;
+
+		// Chytí myš zpátky
+		Input.MouseMode = Input.MouseModeEnum.Captured;
+
+		// Restartuje scénu
+		GetTree().ReloadCurrentScene();
+	}
+
+	private void OnQuitButtonPressed()
+	{
+		GetTree().Quit();
+	}
+}
